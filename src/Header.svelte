cfda11170bf5d034f832522f2cd2eaf1ffe5824b
LucasJC <lucas.couchot@gmail.com>
2021-05-05 15:02:46 -0300

    Improve components structure

---
+++ src/Header.svelte
@@ -0,0 +1,8 @@
+<main>
+	<section class="hero">
+		<div class="hero-body">
+	    <p class="title">RPlanet Staking Configuration</p>
+	    <p class="subtitle">Online staking configuration explorer for <a href="https://rplanet.io" target="_blank">RPlanet</a> game.</p>
+		</div>
+	</section>
+</main>
