6f9cfa01e8d1c7a8942432392dcadff55e9300f9
Fran Guijarro <franleplant@gmail.com>
2021-05-24 22:40:52 -0300

    Implement reusable components, simplify abstract

--- src/PassiveIncome.svelte
+++ src/PassiveIncome.svelte
@@ -1,117 +1,33 @@
 <script lang="ts">
-  import { format } from "./format";
   import { aetherPrice, miningPower, waxPrice } from "./store";
+  import Table from "./PassiveIncomeTable.svelte";
 
-  let aetherInWax: number = 0.0;
-  let aetherInUSD: number = 0.0;
+  let tables: Array<{ label: string; mp: number }>;
 
-  $: if ($miningPower) {
-    aetherInWax = $miningPower * $aetherPrice;
-    aetherInUSD = aetherInWax * $waxPrice;
-  }
+  $: tables = [
+    { label: "Aether", mp: $miningPower },
+    { label: "Wax", mp: $miningPower * $aetherPrice },
+    { label: "USD", mp: $miningPower * $aetherPrice * $waxPrice },
+  ];
 </script>
 
 <main>
   <div class="section">
     {#if $miningPower}
       <p class="subtitle">Passive income rates:</p>
+
       <div class="columns">
-        <div class="column">
-          <p
-            class="subtitle has-text-centered"
-            data-tooltip="Aether calculated income."
-          >
-            Aether
-          </p>
-          <table
-            class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
-          >
-            <tr>
-              <th>Period</th>
-              <th>Amount</th>
-            </tr>
-            <tr>
-              <td>hourly</td>
-              <td>{format($miningPower)}</td>
-            </tr>
-            <tr>
-              <td>daily</td>
-              <td>{format($miningPower * 24)}</td>
-            </tr>
-            <tr>
-              <td>weekly</td>
-              <td>{format($miningPower * 24 * 7)}</td>
-            </tr>
-            <tr>
-              <td>monthly</td>
-              <td>{format($miningPower * 24 * 30)}</td>
-            </tr>
-          </table>
-        </div>
-        <div class="column">
-          <p
-            class="subtitle has-text-centered"
-            data-tooltip="Wax calculated income. Defaults to 0 when prices are not available."
-          >
-            Wax
-          </p>
-          <table
-            class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
-          >
-            <tr>
-              <th>Period</th>
-              <th>Amount</th>
-            </tr>
-            <tr>
-              <td>hourly</td>
-              <td>{format(aetherInWax)}</td>
-            </tr>
-            <tr>
-              <td>daily</td>
-              <td>{format(aetherInWax * 24)}</td>
-            </tr>
-            <tr>
-              <td>weekly</td>
-              <td>{format(aetherInWax * 24 * 7)}</td>
-            </tr>
-            <tr>
-              <td>monthly</td>
-              <td>{format(aetherInWax * 24 * 30)}</td>
-            </tr>
-          </table>
-        </div>
-        <div class="column">
-          <p
-            class="subtitle has-text-centered"
-            data-tooltip="USD calculated income. Defaults to 0 when prices are not available."
-          >
-            USD
-          </p>
-          <table
-            class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
-          >
-            <tr>
-              <th>Period</th>
-              <th>Amount</th>
-            </tr>
-            <tr>
-              <td>hourly</td>
-              <td>{format(aetherInUSD)}</td>
-            </tr>
-            <tr>
-              <td>daily</td>
-              <td>{format(aetherInUSD * 24)}</td>
-            </tr>
-            <tr>
-              <td>weekly</td>
-              <td>{format(aetherInUSD * 24 * 7)}</td>
-            </tr>
-            <tr>
-              <td>monthly</td>
-              <td>{format(aetherInUSD * 24 * 30)}</td>
-            </tr>
-          </table>
-        </div>
+        {#each tables as table}
+          <div class="column">
+            <p
+              class="subtitle has-text-centered"
+              data-tooltip="Aether calculated income."
+            >
+              {table.label}
+            </p>
+            <Table miningPower={table.mp} />
+          </div>
+        {/each}
       </div>
     {/if}
   </div>
