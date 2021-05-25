3de4821aa3532f54910d2f701e09244ab88a7cea
Fran Guijarro <franleplant@gmail.com>
2021-05-24 22:57:03 -0300

    land yield

---
+++ src/am/index.ts
@@ -0,0 +1,14 @@
+//import {
+//Sale,
+//SaleSort,
+//SortOrder,
+//SaleState,
+//SaleParams,
+//} from "atomicmarket/build/API/Explorer/Types";
+import { ExplorerApi } from "atomicmarket";
+
+const URL = "https://wax.api.atomicassets.io";
+const NAMESPACE = "atomicmarket";
+const api = new ExplorerApi(URL, NAMESPACE, {});
+
+export default api;
