f5f8f4a857d0902fa0466140b44c9ca24906fd73
LucasJC <lucas.couchot@gmail.com>
2021-05-27 02:05:13 -0300

    Add basic non yet function menu

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,20 +1,35 @@
 <script lang="ts">
   import { nightMode } from "../domain/store";
+import NightModeToggle from "./NightModeToggle.svelte";
 </script>
 
 <main>
   <section class="hero">
-    <div class="hero-body">
-      <p class="title">
-        RP Whale <span class:night-whale={$nightMode}>🐳</span>
-      </p>
-      <p class="subtitle">
-        Unofficial staking calculator utility for <a
-          href="https://rplanet.io"
-          target="_blank">RPlanet</a
-        > game.
-      </p>
+    <div class="hero-body columns">
+      <div class="column">
+        <p class="title">
+          RP Whale <span class:night-whale={$nightMode}>🐳</span>
+        </p>
+        <p class="subtitle">
+          Unofficial staking calculator utility for <a
+            href="https://rplanet.io"
+            target="_blank">RPlanet</a
+          > game.
+        </p>
+      </div>
+      <div class="column">
+        <div class="tabs is-right">
+          <ul>
+            <li><a>Calculator</a></li>
+            <li><a>Staking</a></li>
+            <li><a>Glossary</a></li>
+            <li><a>Contact</a></li>
+            <NightModeToggle />
+          </ul>
+        </div>
+      </div>
     </div>
+
   </section>
 </main>
 
