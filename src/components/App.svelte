89c7c481b02dae174151e8e7d9b7003e3a6bec13
LucasJC <lucas.couchot@gmail.com>
2021-06-26 15:46:03 -0300

    adsense test

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -19,38 +19,36 @@
   import { ASSET_SEARCH_KEY } from "../domain/asset-staking";
 </script>
 
-<main>
-  <GoogleAnalytics properties={["G-1WNKLF5N10"]} />
-  <div class="container">
-    <Router>
-      <Header />
-      <Route path="/">
-        <AccountInput account={getFromSearch(ACCOUNT_SEARCH_KEY)} />
-        {#if $userStore.account}
-          <GoUpButton />
-          <Summary />
-          <Currencies />
-          <AccountStaking />
-          <PassiveIncome />
-          <AccountHoldings />
-          <LandsSummary />
-        {:else}
-          <Currencies />
-        {/if}
-      </Route>
+<GoogleAnalytics properties={["G-1WNKLF5N10"]} />
+<div class="container">
+  <Router>
+    <Header />
+    <Route path="/">
+      <AccountInput account={getFromSearch(ACCOUNT_SEARCH_KEY)} />
+      {#if $userStore.account}
+        <GoUpButton />
+        <Summary />
+        <Currencies />
+        <AccountStaking />
+        <PassiveIncome />
+        <AccountHoldings />
+        <LandsSummary />
+      {:else}
+        <Currencies />
+      {/if}
+    </Route>
 
-      <Route path="asset-staking">
-        <AssetYield assetId={getFromSearch(ASSET_SEARCH_KEY)} />
-      </Route>
+    <Route path="asset-staking">
+      <AssetYield assetId={getFromSearch(ASSET_SEARCH_KEY)} />
+    </Route>
 
-      <Route path="pools">
-        <Pools filters={$poolFilters} />
-      </Route>
+    <Route path="pools">
+      <Pools filters={$poolFilters} />
+    </Route>
 
-      <Route path="donation">
-        <Donations />
-      </Route>
-    </Router>
-  </div>
-  <Footer />
-</main>
+    <Route path="donation">
+      <Donations />
+    </Route>
+  </Router>
+</div>
+<!--<Footer />-->
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import "declarations.d.ts";
 import App from "./components/App.svelte";
+import Footer from "./components/Footer.svelte";
 import "./styles.css";
 
 const app = new App({
-  target: document.body,
+  target: document.querySelector("#content")!,
+  props: {},
+});
+
+const footer = new Footer({
+  target: document.querySelector("#footer")!,
   props: {},
 });
 
