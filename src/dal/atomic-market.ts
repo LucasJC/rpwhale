a0bd8ff286791d91d9dd0b143e170a19ee0e3a49
Lucas Jeremías Couchot <lucas.couchot@gmail.com>
2021-07-16 20:55:03 -0300

    Temporarily change atomic assets API url to a mirror (#59)
    Quick fix to change atomicassets URL to a working mirror

--- src/dal/atomic-market.ts
+++ src/dal/atomic-market.ts
@@ -1,11 +1,13 @@
 export type { ListingAsset } from "atomicmarket/build/API/Explorer/Types";
 import { ExplorerApi } from "atomicmarket";
 
-const ATOMIC_ASSETS_HOST = "https://wax.api.atomicassets.io";
+//const ATOMIC_ASSETS_HOST = "https://wax.api.atomicassets.io";
+// EOS Amsterdam mirror
+const ATOMIC_ASSETS_HOST = "https://wax-aa.eu.eosamsterdam.net";
 
 export function atomicMarket(): ExplorerApi {
   if (window) {
     (window as any).global = window;
   }
-  return new ExplorerApi(ATOMIC_ASSETS_HOST, "atomicmarket", {});
+  return new ExplorerApi(ATOMIC_ASSETS_HOST, "atomicassets", {});
 }
